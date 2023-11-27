ae347060a2114a812a5210a2a3395074a0dca391
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2023-11-28 00:57:07 +0530

    logic 2 for clone LL

--- linked list/cloneALinkedList.cpp
+++ linked list/cloneALinkedList.cpp
@@ -66,3 +66,91 @@
 //     }
 
 // };
+
+
+// //*********Approach 2**********
+// class Solution
+// {
+//     private:
+    
+//     void insertAtTail(Node*&head,Node*& tail,int data)
+//     {
+//         Node *newNode= new Node(data);
+//         if(head==NULL)
+//         {
+//             head=newNode;
+//             tail=newNode;
+//             return;
+//         }
+        
+//         else
+//         {
+//             tail->next=newNode;
+//             tail=newNode;
+//         }
+//     }
+    
+    
+//     public:
+//     Node *copyList(Node *head)
+//     {
+//         Node *cloneHead=NULL;
+//         Node *cloneTail=NULL;
+        
+//         Node *temp=head;
+        
+//         while(temp!=NULL)
+//         {
+//             insertAtTail(cloneHead,cloneTail,temp->data);
+//             temp=temp->next;
+//         }
+        
+//         Node *oldNode=head;
+//         Node *cloneNode=cloneHead;
+        
+//         while(oldNode!=NULL && cloneNode!=NULL)
+//         {
+//             Node * next=oldNode->next;
+//             oldNode->next=cloneNode;
+//             oldNode=next;
+            
+//             next=cloneNode->next;
+//             cloneNode->next=oldNode;
+//             cloneNode=next;
+            
+//         }
+        
+//         temp=head;
+        
+//         while(temp!=NULL)
+//         {
+//             if(temp->next!=NULL)
+//             {
+//                 temp->next->arb=temp->arb ? temp->arb->next:temp->arb;
+//             }
+            
+//             temp=temp->next->next;
+//         }
+        
+//         oldNode=head;
+//         cloneNode=cloneHead;
+        
+//         while(oldNode!=NULL && cloneNode!=NULL)
+//         {
+//             oldNode->next=cloneNode->next;
+//             oldNode=oldNode->next;
+            
+//             if(oldNode!=NULL)
+//             {
+//                 cloneNode->next=oldNode->next;
+//             }
+//             cloneNode=cloneNode->next;
+            
+//         }
+        
+//        return cloneHead; 
+        
+        
+//     }
+
+// };
