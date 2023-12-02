07c597f8290504b5ca1590854ffc88c52b841d95
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2023-12-03 00:24:26 +0530

    changes

--- stacks/stackArrayImplementation.cpp
+++ stacks/stackArrayImplementation.cpp
@@ -32,19 +32,77 @@ public:
 
     void pop()
     {
+        if (top >= 0)
+        {
+            top--;
+        }
+
+        else
+        {
+            cout << endl
+                 << "Stack underflow";
+        }
     }
 
     int peek()
     {
+        if (top >= 0)
+        {
+            return (arr[top]);
+        }
+
+        else
+        {
+            cout << "The stack is empty";
+            return -1;
+        }
     }
 
     bool isEmpty()
     {
+        if (top == -1)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
     }
 };
 
 int main()
 {
+    Stack st(5);
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+
+    cout << st.peek()<<endl;
+
+    st.pop();
+    cout << st.peek()<<endl;
+
+    st.pop();
+    cout << st.peek()<<endl;
+
+    st.pop();
+    cout << st.peek()<<endl;
+
+    st.pop();
+    cout << st.peek()<<endl;
+
+    if(st.isEmpty())
+    {
+        cout<<"Khali hoon bhai";
+    }
+
+    else{
+        cout<<"Mere me abhi data hai";
+    }
+
 
     return 0;
 }
