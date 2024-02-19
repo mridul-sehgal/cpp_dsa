463a0cd677b3c86cf1f80788dd28e3698b1ca477
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-02-20 03:06:09 +0530

    largest bst in a binary tree

--- Binary Search Tree/merge2BST.cpp
+++ Binary Search Tree/merge2BST.cpp
@@ -60,5 +60,125 @@
 
 
 
-
 // **************************************************** Approach 2 **********************************************
+
+// void convertIntoSortedDLL(Node *root, Node *head)
+// {
+//     if (root == NULL)
+//     {
+//         return NULL;
+//     }
+
+//     convertIntoSortedDLL(root->right, head);
+//     root->right = head;
+//     if (head != NULL)
+//     {
+//         head->left = root;
+//     }
+
+//     head = root;
+//     convertIntoSortedDLL(root->leftt, head);
+// }
+
+// Node *mergeLL(Node *head1, Node *head2)
+// {
+//     Node *head = NULL;
+//     Node *tail = NULL;
+
+//     while (head1 != NULL && head2 != NULL)
+//     {
+//         if (head1->data < head2->data)
+//         {
+//             if (head == NULL)
+//             {
+//                 head = head1;
+//                 tail = head1;
+//                 head1 = head1->right;
+//             }
+//             else
+//             {
+//                 tail->right = head1;
+//                 head->left = tail;
+//                 tail = head1;
+//                 head1 = head1->right;
+//             }
+//         }
+//         else
+//         {
+//             if (head == NULL)
+//             {
+//                 head = head2;
+//                 tail = head2;
+//                 head1 = head2->right;
+//             }
+//             else
+//             {
+//                 tail->right = head2;
+//                 head->left = tail;
+//                 tail = head2;
+//                 head2 = head2->right;
+//             }
+//         }
+//     }
+
+//     while (head1 != NULL)
+//     {
+//         tail->right = head1;
+//         head->left = tail;
+//         tail = head1;
+//         head1 = head1->right;
+//     }
+
+//     while (head2 != NULL)
+//     {
+//         tail->right = head2;
+//         head->left = tail;
+//         tail = head2;
+//         head2 = head2->right;
+//     }
+
+//     return head;
+// }
+
+// int countNode(Node *head)
+// {
+//     int cnt = 0;
+//     Node *temp = head;
+//     while (temp->right != NULL)
+//     {
+//         cnt++;
+//         temp = temp->right;
+//     }
+//     return cnt;
+// }
+
+// Node *sortedLLToBst(Node *&head, int n)
+// {
+//     if (n <= 0 || head == NULL)
+//         return NULL;
+
+//     Node *leftTree = sortedLLToBst(head, n / 2);
+//     Node *root = head;
+//     root->left = leftTree;
+
+//     head = head->right;
+
+//     root->right = sortedLLToBst(head, n - (n / 2) - 1);
+//     return root;
+// }
+
+// Node *mergeBst(Node *root1, Node *root2)
+// {
+//     Node *head1 = NULL;
+//     convertIntoSortedDLL(root1, head1);
+//     head1->left = NULL;
+
+//     Node *head2 = NULL;
+//     convertIntoSortedDLL(root2, head2);
+//     head2->left = NULL;
+
+//     Node *head = mergeLL(head1, head2);
+
+//     int n = countNode(head);
+//     return sortedLLToBst(head, n);
+// }
