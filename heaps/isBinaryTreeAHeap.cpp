389f71cb849dfeb3f0cff261b4bb54496043a9c5
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-02-25 04:00:32 +0530

    is Binary tree a heap , merge 2heap

---
+++ heaps/isBinaryTreeAHeap.cpp
@@ -0,0 +1,75 @@
+// class Solution
+// {
+// public:
+//     int countNode(struct Node *root)
+//     {
+//         if (root == NULL)
+//             return 0;
+
+//         int ans = 1 + countNode(root->left) + countNode(root->right);
+//         return ans;
+//     }
+
+//     bool isCBT(struct Node *root, int index, int cnt)
+//     {
+//         if (root == NULL)
+//         {
+//             return true;
+//         }
+
+//         if (index >= cnt)
+//         {
+//             return false;
+//         }
+
+//         else
+//         {
+//             bool left = isCBT(root->left, 2 * index + 1, cnt);
+//             bool right = isCBT(root->right, 2 * index + 2, cnt);
+//             return (left && right);
+//         }
+//     }
+
+//     bool isMaxOrder(struct Node *root)
+//     {
+//         if (root->left == NULL && root->right == NULL)
+//         {
+//             return true;
+//         }
+
+//         if (root->right == NULL)
+//         {
+//             return (root->data > root->left->data);
+//         }
+
+//         else
+//         {
+//             bool left = isMaxOrder(root->left);
+//             bool right = isMaxOrder(root->right);
+//             if (left && right && (root->data > root->left->data && root->data > root->right->data))
+//             {
+//                 return true;
+//             }
+
+//             else
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     bool isHeap(struct Node *root)
+//     {
+//         int index = 0;
+//         int totalCount = countNode(root);
+//         if (isCBT(root, index, totalCount) && isMaxOrder(root))
+//         {
+//             return true;
+//         }
+
+//         else
+//         {
+//             return false;
+//         }
+//     }
+// };
