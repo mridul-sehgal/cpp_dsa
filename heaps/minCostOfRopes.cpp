cac7393eee3e3f223f82321ba1c723e47d147428
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-02-26 00:49:07 +0530

    min heap cost

---
+++ heaps/minCostOfRopes.cpp
@@ -0,0 +1,28 @@
+// class Solution
+// {
+// public:
+//     // Function to return the minimum cost of connecting the ropes.
+//     long long minCost(long long arr[], long long n)
+//     {
+//         priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+//         long long cost = 0;
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             pq.push(arr[i]);
+//         }
+
+//         while (pq.size() > 1)
+//         {
+//             long long a = pq.top();
+//             pq.pop();
+//             long long b = pq.top();
+//             pq.pop();
+
+//             pq.push(a + b);
+//             cost += (a + b);
+//         }
+//         return cost;
+//     }
+// };
