e3e4ca3895c088f208623ec66dcec41e3c9999e1
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-02-28 23:48:11 +0530

    merge k sorted linked lists

---
+++ heaps/mergeKSortedLL.cpp
@@ -0,0 +1,55 @@
+// class compare
+// {
+// public:
+//     bool operator()(Node *a, Node *b)
+//     {
+//         return a->data > b->data;
+//     }
+// };
+
+// Node *mergeKLists(vector<Node *> &listArray)
+// {
+//     priority_queue<Node *, vector<Node *>, compare> minHeap;
+//     int k = listArray.size();
+
+//     if (k == 0)
+//     {
+//         return NULL;
+//     }
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         if (listArray[i] != NULL)
+//         {
+//             minHeap.push(listArray[i]);
+//         }
+//     }
+
+//     Node *head = NULL;
+//     Node *tail = NULL;
+
+//     while (minHeap.size() > 0)
+//     {
+//         Node *temp = minHeap.top();
+//         minHeap.pop();
+
+//         if (temp->next != NULL)
+//         {
+//             minHeap.push(temp->next);
+//         }
+
+//         if (head == NULL)
+//         {
+//             head = temp;
+//             tail = temp;
+//         }
+
+//         else
+//         {
+//             tail->next = temp;
+//             tail = temp;
+//         }
+//     }
+
+//     return head;
+// }
