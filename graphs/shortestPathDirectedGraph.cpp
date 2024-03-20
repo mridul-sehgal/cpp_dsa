66f50dd0b35bbf5d0f566f2c75495ef8cb3001e9
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-03-20 22:36:57 +0530

    question

--- graphs/shortestPathDirectedGraph.cpp
+++ graphs/shortestPathDirectedGraph.cpp
@@ -13,38 +13,73 @@ public:
 
     void printAdj()
     {
-        for(auto i:adj)
+        for (auto i : adj)
         {
-            cout<<i.first<<" -> ";
-            for(auto j:i.second)
+            cout << i.first << " -> ";
+            for (auto j : i.second)
             {
-                cout<<"("<<j.first<<","<<j.second<<"), ";
+                cout << "(" << j.first << "," << j.second << "), ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 
+    void dfs(int node,unordered_map<int,bool>&visited,stack<int>&s)
+    {
+        visited[node]=1;
+        for(auto i : adj[node])
+        {
+            if(!visited[i.first])
+            {
+                dfs(i.first,visited,s);
+            }
+        }
+    }
+
+    void getShortestPath()
 };
 
 int main()
 {
     Graph g;
-    g.addEdge(0,1,5);
-    g.addEdge(0,2,3);
-    g.addEdge(1,2,2);
-    g.addEdge(1,3,6);
-    g.addEdge(2,3,7);
-    g.addEdge(2,4,4);
-    g.addEdge(2,5,2);
-    g.addEdge(3,4,-1);
-    g.addEdge(4,5,-2);
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 2, 2);
+    g.addEdge(1, 3, 6);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 4, 4);
+    g.addEdge(2, 5, 2);
+    g.addEdge(3, 4, -1);
+    g.addEdge(4, 5, -2);
 
     g.printAdj();
 
-    int n=6;
+    int n = 6;
+    unordered_map<int,bool>visited;
+    stack<int>s;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+        {
+            g.dfs(i,visited,s);
+        }
+    }
+
+    int src=1;
+    vector<int>dist(n);
     for(int i=0;i<n;i++)
     {
-        
+        dist[i]=INT_MAX;
+    }
+
+    dist[src]=0;
+    while(!s.empty())
+    {
+        int top=s.top();
+        s.pop();
+
+        for(auto i:)
     }
     return 0;
 }
