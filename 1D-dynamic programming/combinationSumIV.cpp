c68fbced3d228acb8eede86aa6dd16026aca77d2
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-04-27 16:49:21 +0530

    dp questions

---
+++ 1D-dynamic programming/combinationSumIV.cpp
@@ -0,0 +1,83 @@
+// ************ Recurrsive Code *************
+
+
+// #include <bits/stdc++.h> 
+
+// int solve(vector<int> &num, int tar)
+// {
+//     if(tar<0)
+//         return 0;
+//     if(tar==0)
+//         return 1;
+
+//     int ans=0;
+//     for(int i=0;i<num.size();i++)
+//     {
+//         ans+=solve(num,tar-num[i]);
+//     }
+//     return ans;
+// }
+
+// int findWays(vector<int> &num, int tar)
+// {
+//     return solve(num,tar);
+// }
+
+
+
+
+// *************** Memoization ***************
+
+
+// #include <bits/stdc++.h> 
+
+// int solve(vector<int> &num, int tar,vector<int>&dp )
+// {
+//     if(tar<0)
+//         return 0;
+//     if(tar==0)
+//         return 1;
+//     if(dp[tar]!=-1)
+//         return dp[tar];
+//     int ans=0;
+//     for(int i=0;i<num.size();i++)
+//     {
+//         ans+=solve(num,tar-num[i],dp);
+//     }
+//     dp[tar]=ans;
+//     return dp[tar];
+// }
+
+// int findWays(vector<int> &num, int tar)
+// {
+//     vector<int>dp(tar+1,-1);
+//     return solve(num,tar,dp);
+// }
+
+
+// ************** Tabulation Code ****************
+
+// #include <bits/stdc++.h> 
+
+// int solve(vector<int> &num, int tar )
+// {
+//     vector<int>dp(tar+1,0);
+    
+//     dp[0]=1;
+
+//     for(int i=1;i<=tar;i++)
+//     {
+//          for(int j=0;j<num.size();j++)
+//         {
+//             if((i-num[j])>=0)
+//            dp[i]+=dp[i-num[j]];
+//         }
+//     }
+//    return dp[tar];
+// }
+
+// int findWays(vector<int> &num, int tar)
+// {
+    
+//     return solve(num,tar);
+// }
---
+++ 1D-dynamic programming/perfectSquareSum.cpp
@@ -0,0 +1,70 @@
+// **********Recurssive **********
+
+// int solve(int n)
+// {
+//     if (n == 0)
+//         return 0;
+
+//     int ans = n;
+//     for (int i = 1; i * i <= n; i++)
+//     {
+//         int temp = i * i;
+//         ans = min(ans, 1 + solve(n - temp, dp));
+//     }
+
+//     return ans;
+// }
+// int MinSquares(int n)
+// {
+
+//     return solve(n, dp);
+// }
+
+// ********** Memoization **********
+
+// int solve(int n,vector<int>&dp)
+// 	{
+// 	     if(n==0)
+// 	    return 0;
+
+// 	    if(dp[n]!=-1)
+// 	    return dp[n];
+
+// 	    int ans=n;
+// 	    for(int i=1;i*i<=n;i++)
+// 	    {
+// 	        int temp=i*i;
+// 	        ans=min(ans,1+solve(n-temp,dp));
+// 	    }
+// 	    dp[n]=ans;
+// 	    return dp[n];
+// 	}
+// 	int MinSquares(int n)
+// 	{
+// 	   vector<int>dp(n+1,-1);
+// 	   return solve(n,dp);
+// 	}
+
+// ************ Tabulation ************
+
+// int solve(int n)
+// 	{
+// 	    vector<int>dp(n+1,INT_MAX);
+// 	    dp[0]=0;
+
+// 	    for(int i=1;i<=n;i++ )
+// 	    {
+// 	        for(int j=1;j*j<=n;j++)
+// 	        {
+// 	            int temp=j*j;
+// 	            if(i-temp>=0)
+// 	            dp[i]=min(dp[i],1+dp[i-temp]);
+// 	        }
+// 	    }
+// 	    return dp[n];
+
+// 	}
+// 	int MinSquares(int n)
+// 	{
+// 	   return solve(n);
+// 	}
